9c374161afbc9f99b19a3a13676fc6f93530419d
AmericoCh <americochulluncuy@gmail.com>
2026-05-09 11:01:44 -0500

    clase 5 - actualizado

--- 05-ejercicios-integradores/patronX.cpp
+++ 05-ejercicios-integradores/patronX.cpp
@@ -9,7 +9,13 @@ Imprimir,para n = 7
  *   *
 *     *
 
-
+* B B B B B *  --> i == j    i == 1 j == 7  --> i + j == 8 == n + 1
+B * B B B * B  --> i == j    i == 2 j == 6  ---> i + j == 8 == n + 1 
+B B * B * B B
+B B B * B B B
+B B * B * B B
+B * B B B * B
+* B B B B B *
 */
 
 #include <iostream>
@@ -17,10 +23,24 @@ using namespace std;
 
 int main(){
     // Datos
-
-
+    int n;
+    cout << "Ingrese un valor de n para formar la X: ";
+    cin >> n;
 
     // Procesamiento y salida
+
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            if(i == j || i + j == n + 1) {  // RAZONAMIENTO
+                cout << "*";
+            }else {
+                cout << " ";
+            }
+            
+        }
+        cout << endl;
+    }
+
 
     return 0;
 }
